5a59ff8d1e19f6f2d8eb7517ece99aec1c63c88e
C.J. May <doctormay6@gmail.com>
2021-04-15 10:34:52 -0500

    implemented parsing import statement and deduping

--- pyurify.py
+++ pyurify.py
@@ -11,7 +11,7 @@ def pyurify(script, imported=None):
         purePython (Str): original python script with all import statements expanded to be included locally
     """
     if imported is None:
-        imported = []  # intentionally made mutable for recursion (don't want to mutate default arg value)
+        imported = {}  # intentionally mutable object for recursion (don't want to mutate default arg value)
     purePython = ""
     for line in script.split("\n"):
         if line.strip()[:7] == "import " or (line.strip()[:5] == "from " and "import " in line):
@@ -29,13 +29,32 @@ def pyurify(script, imported=None):
 
 def expandImport(importStatement, imported):
     """Fetch the code for the import and return all of it"""
-    # TODO: parse import statement and extract module name
-
-    # TODO: if import is already in 'imported' arg (code exists in purePython already), return "\n"
+    # parse import statement to extract module name and submodules
+    splitImport = importStatement.split()
+    if splitImport[0] == "import":  # ex. 'import <module>'
+        # TODO: account for dotted submodules and local package references
+        moduleName = splitImport[1]
+        submodules = []
+    else:                           # ex. 'from <module> import <submodules>
+        moduleName = splitImport[1]
+        submodules = splitImport[3:]
+    # if module is already in 'imported' dict, check for duplicate submodules
+    if moduleName in imported:
+        if set(imported[moduleName]) == set(submodules):
+            return "\n"
+        if imported[moduleName]:
+            submodules = list(set(submodules) - set(imported[moduleName]))
+            imported[moduleName] += submodules
+        else:  # entire module already imported
+            return "\n"
+    else:  # add new module to 'imported' dict
+        imported[moduleName] = submodules
 
     # TODO: import module into current namespace
 
     # TODO: use module's __file__ attribute to fetch source code
+    # or
+    # TODO: serialize/pickle module
 
     # TODO: account for sub-imports -- ex. from datetime import datetime
 
